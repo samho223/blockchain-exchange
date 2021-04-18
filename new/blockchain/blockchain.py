0f6e0f17d5c93f2bcb1c736bb49a46ea4b3a797d
samho223 <70087291+samho223@users.noreply.github.com>
2021-04-19 00:12:15 +0800

    Delete new/blockchain directory

--- new/blockchain/blockchain.py
+++
@@ -1,7 +0,0 @@
-from app import app, db
-from app.models import User, Post
-from backend import blockchain1,wallet
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
--- new/blockchain/test.py
+++
@@ -1,8 +0,0 @@
-from backend.wallet import Wallet
-import binascii
-
-
-wallet = Wallet()
-
-private_key = wallet.private_key()
-print(binascii.hexlify(private_key.exportKey(format='DER')).decode('ascii'))
